d85acc08b3e3d79e1b5cccd9653e83e5f39ebfcc
Filipp Gaponenko <psr1919plus21@gmail.com>
2017-07-09 22:47:53 +0300

    :rocket: Build short way.

--- game/helpers/li.js
+++ game/helpers/li.js
@@ -1,16 +1,15 @@
+let source;
+let target;
+let field = '';
 let map = [
-  [{val: '[_]'}, {val: '[_]'}, {val: '[t]'}],
-  [{val: '[_]'}, {val: '[_]'}, {val: '[s]'}],
+  [{val: '[s]'}, {val: '[_]'}, {val: '[_]'}],
   [{val: '[_]'}, {val: '[_]'}, {val: '[_]'}],
+  [{val: '[_]'}, {val: '[t]'}, {val: '[_]'}],
   [{val: '[_]'}, {val: '[_]'}, {val: '[_]'}],
   [{val: '[_]'}, {val: '[_]'}, {val: '[_]'}],
   [{val: '[_]'}, {val: '[_]'}, {val: '[_]'}]
 ]
 
-let source ;
-let target;
-
-let field = '';
 map.forEach((row, index) => {
   row.forEach((cell, i) => {
     cell.x = index;
@@ -22,16 +21,15 @@ map.forEach((row, index) => {
   field += '\n';
 });
 
+
 let targetAcheeved = false;
 let moores = [];
 let mooreStack = [];
 
 function mooreWave(_source, _target, level = 0) {
   if (targetAcheeved || !_target) return moores;
-
   let moore = getMoore(_source, _target, level);
   let {cells} = moore;
-  // console.log('cells', cells);
 
   if (cells.length) {
     cells.forEach((cell) => mooreStack.push(cell));
@@ -41,17 +39,20 @@ function mooreWave(_source, _target, level = 0) {
     targetAcheeved = moore.hasTarget;
     console.log('Target acheeved');
     moores.targetAcheeved = true;
-    moores.push(moore);
+    if (moore.cells.length) {
+      moores.push(moore);
+    }
     return moores;
   }
 
   if (mooreStack.length) {
-    // console.log('run recurcieve');
     let nextCell = mooreStack.shift();
     mooreWave(nextCell, _target, nextCell.steps);
   }
 
-  moores.push(moore);
+  if (moore.cells.length) {
+    moores.push(moore);
+  }
   return moores;
 
 }
@@ -66,23 +67,25 @@ function getMoore(cell, _target, level = 0) {
   if (!cell) {
     return moore;
   }
+
   let {x, y} = cell;
 
-  if (!level && !cell.steps) {
+  if (!level && !('steps' in cell)) {
     let cell = map[x][y];
     cell.steps = 0;
     moore.cells.push(cell);
+    return moore;
   }
 
   let CELLS = {
     N: map[x] ? map[x][y-1] : null,
-    // NE: map[x+1] ? map[x+1][y-1] : null,
+    // NE: map[x+1] ? map[x+1][y-1] : null, -- Moore
     E: map[x+1] ? map[x+1][y] : null,
-    // SE: map[x+1] ? map[x+1][y+1] : null,
+    // SE: map[x+1] ? map[x+1][y+1] : null,  -- Moore
     S: map[x] ? map[x][y+1] : null,
-    // SW: map[x-1] ? map[x-1][y+1] : null,
+    // SW: map[x-1] ? map[x-1][y+1] : null,  -- Moore
     W: map[x-1] ? map[x-1][y] : null
-    // NW: map[x-1] ? map[x-1][y-1] : null,
+    // NW: map[x-1] ? map[x-1][y-1] : null, -- Moore
   }
 
   for (item in CELLS) {
@@ -96,20 +99,64 @@ function getMoore(cell, _target, level = 0) {
       moore.cells.push(cell);
     }
   };
-  // console.log(moore);
 
   moore.level++;
   return moore;
 }
 
+let _way = [];
+
+function buildWay(wayMap, target) {
+  if (wayMap.length) {
+    let currentLevel = wayMap.shift();
+    if (currentLevel.hasTarget) {
+      let targetCell = currentLevel.cells.filter((cell) => {return cell.isTarget})[0];
+      _way.push(targetCell);
+      buildWay(wayMap, targetCell);
+    } else {
+      let CELLS = [];
+      let {x, y} = target;
+      let {cells} = currentLevel;
+      let nextCell;
+
+      cells.forEach((cell) => {
+        if (cell.x === x && cell.y === (y - 1)) CELLS.push(cell);
+        if (cell.x === x && cell.y === (y + 1)) CELLS.push(cell);
+        if (cell.x === (x - 1) && cell.y === y) CELLS.push(cell);
+        if (cell.x === (x + 1) && cell.y === y) CELLS.push(cell);
+      });
+
+      let minLevel = currentLevel.level;
+      CELLS.forEach((cell) => {
+        Math.min(minLevel, cell.steps);
+      });
+
+      nextCell = CELLS.filter((cell) => {
+        return cell.steps === minLevel;
+      })[0];
+
+      nextCell = nextCell || target;
+
+      _way.push(nextCell);
+      buildWay(wayMap, nextCell);
+    }
+  }
+}
+
 
 console.log(field);
 let moore = mooreWave(source, target);
-// console.log(moore);
+let _moore = [].concat(moore);
+
+let way = buildWay(_moore);
 
 moore.forEach((row, index) => {
   row.cells.forEach((cell, i) => {
-    map[cell.x][cell.y].val = cell.isTarget ? `[t]` : `[${cell.steps}]`;
+    if (cell.x === source.x && cell.y === source.y) {
+      map[cell.x][cell.y].val = '[s]';
+    } else {
+      map[cell.x][cell.y].val = cell.isTarget ? `[t]` : `[${cell.steps}]`;
+    }
   });
 });
 
@@ -124,4 +171,24 @@ map.forEach((row, index) => {
 });
 
 console.log(field);
-console.log(mooreStack);
+
+
+// WAY
+_way.forEach((cell) => {
+    let content = map[cell.x][cell.y].val;
+    if (content !== '[s]' && content !== '[t]') {
+      map[cell.x][cell.y].val = '[x]';
+    }
+});
+
+field = '';
+map.forEach((row, index) => {
+  row.forEach((cell, i) => {
+    cell.x = index;
+    cell.y = i;
+    field += cell.val;
+  });
+  field += '\n';
+});
+
+console.log(field);
